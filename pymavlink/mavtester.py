a72362f60b736cfc7b882d9b40275202d43b1bb2
Jens Nielsen <nens90@gmail.com>
2014-11-02 13:27:55 +0100

    ..

--- pymavlink/mavtester.py
+++ pymavlink/mavtester.py
@@ -7,14 +7,15 @@ test mavlink messages
 import sys, struct, time, os
 from curses import ascii
 
-from pymavlink import mavtest, mavutil
+from pymavlink import mavutil
+from dialects.v10 import mavlinkv10 as mavlink
 
 from argparse import ArgumentParser
 parser = ArgumentParser(description=__doc__)
 
-parser.add_argument("--baudrate", type=int,
-                  help="master port baud rate", default=115200)
-parser.add_argument("--device", required=True, help="serial device")
+parser.add_argument("-b", type=int,
+                  help="master port baud rate", default=57600)
+parser.add_argument("-d", required=True, help="serial device")
 parser.add_argument("--source-system", dest='SOURCE_SYSTEM', type=int,
                   default=255, help='MAVLink source system for this GCS')
 args = parser.parse_args()
@@ -26,11 +27,11 @@ def wait_heartbeat(m):
     print("Heartbeat from APM (system %u component %u)" % (m.target_system, m.target_system))
 
 # create a mavlink serial instance
-master = mavutil.mavlink_connection(args.device, baud=args.baudrate, source_system=args.SOURCE_SYSTEM)
+master = mavutil.mavlink_connection(args.d, baud=args.b, source_system=args.SOURCE_SYSTEM)
 
 # wait for the heartbeat msg to find the system ID
 wait_heartbeat(master)
 
-print("Sending all message types")
-mavtest.generate_outputs(master.mav)
-
+#print("Sending all message types")
+#mavtest.generate_outputs(master.mav)
+print(master)
