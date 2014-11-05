3ed7a50f0577e1c3cbc87c6f3c962033e85b38f8
Jens Nielsen <nens90@gmail.com>
2014-11-05 09:41:26 +0100

    ..

--- pymavlink/formation.py
+++ pymavlink/formation.py
@@ -1,28 +1,63 @@
 #!/usr/bin/env python
-import shlex
+import sys, struct, time, os, shlex
 import numpy as np
+from curses import ascii
+from time import sleep
+from pymavlink import mavutil
+from dialects.v10 import mavlinkv10 as mavlink
+from pymavlink import mav_formation as formation
 
+from argparse import ArgumentParser
+parser = ArgumentParser(description=__doc__)
+
+parser.add_argument("-b", type=int,
+                  help="master port baud rate", default=57600)
+parser.add_argument("-d", required=False, help="serial device", default="/dev/ttyUSB0")
+parser.add_argument("--source-system", dest='SOURCE_SYSTEM', type=int,
+                  default=255, help='MAVLink source system for this GCS')
+args = parser.parse_args()
+
+# create a mavlink serial instance
+xbee = mavutil.mavlink_connection(args.d, baud=args.b, source_system=args.SOURCE_SYSTEM)
+
+# wait_heartbeat(xbee)
 
 while True:
 	input = raw_input("FORMATION >> ")
 	ans = shlex.split(input)
 	dim = len(ans)
 
-	if ans[0] == 'start':
+	if ans[0] == 'arm' :
+		ARM = True
+
+		if dim > 1 :
+			target_system = int(ans[1])
+		else:
+			target_system = QUAD_FORMATION_ID_ALL
+
+		quad_arm_disarm(xbee, target_system, ARM)
+
+
+	elif ans[0] == 'start':
 		if dim > 1 :
 			target_system = int(ans[1])
 			QUAD_CMD = int(ans[2])
 		else:
-			target_system = QUAD_CMD = 0
+			target_system = QUAD_CMD_START
 		print ("1 - Start script - target_system: %u  CMD: %u" % (target_system, QUAD_CMD))
+
+		# Execute the given script
+
+
+
 
 	elif ans[0] == 'stop':
 		if dim	> 1 :
 			target_system = ans[1]
 			target_system = int(ans[1])
-		else:
+		else :
 			target_system = 0
-		print ("2 - Stopping script - target_system: %u" %(target_system)
+		print ("2 - Stopping script - target_system: %u" %(target_system))
 
 	elif ans[0] == 'log':
 		print "3 - logging"
