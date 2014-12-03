894bae23f752e31916a4d24b1824557dc80ca55e
Jens Nielsen <nens90@gmail.com>
2014-12-03 09:55:43 +0100

    JN

--- pymavlink/multithreadingv2.py
+++ pymavlink/multithreadingv2.py
@@ -42,12 +42,12 @@ class MatlabUDPHandler(SocketServer.BaseRequestHandler):
 	# print "%s wrote:" % self.client_address[0]
 	numOfValues = len(data) / 8
 	vicn = struct.unpack('>' + 'd' * numOfValues, data)
-	# index = vicn[0]
-	# x = ([ vicn[1],vicn[4],vicn[7],vicn[10],vicn[13],vicn[16],vicn[19],vicn[22],vicn[25],vicn[28] ])
-	# y = ([ vicn[2],vicn[5],vicn[8],vicn[11],vicn[14],vicn[17],vicn[20],vicn[23],vicn[26],vicn[29] ])
-	# z = ([ vicn[3],vicn[6],vicn[9],vicn[12],vicn[15],vicn[18],vicn[21],vicn[24],vicn[27],vicn[30] ])
+	
 	# index, x, y, z = vicn[0], vicn[1], vicn[2], vicn[3]
 	index = vicn[0]
+
+	# Find the number of quadrotors
+	no_of_quad = (len(vicn)-1)/3
 
 	for i in xrange(1,no_of_quad):
 		x = vicn[3*i-2]
